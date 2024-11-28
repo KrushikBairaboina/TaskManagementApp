7c721b00ddcfcfe67d2549e0ad27d9eb25678d0d
krushik bairaboina <113887483+KrushikBairaboina@users.noreply.github.com>
2024-11-28 23:01:31 +0530

    Update T

--- backend/Models/Task.js
+++ backend/Models/Task.js
@@ -13,13 +13,13 @@ const TaskSchema = new mongoose.Schema({
     trim: true
   },
   dueDate: {
-    type: String, // Store as a String in 'YYYY-MM-DD' format
+    type: String, 
     required: true,
     set: (value) => {
-      // If the user provides a date with time, convert it to 'YYYY-MM-DD' format
+      
       const date = new Date(value);
-      // Format the date as 'YYYY-MM-DD'
-      return date.toISOString().split('T')[0]; // This will return the date part only
+
+      return date.toISOString().split('T')[0]; 
     }
   },
   status: {
@@ -28,10 +28,9 @@ const TaskSchema = new mongoose.Schema({
     default: 'Pending'
   }
 }, {
-  timestamps: true // Automatically adds createdAt and updatedAt
+  timestamps: true 
 });
 
-// Model creation
 const Task = mongoose.model('Task', TaskSchema);
 
 module.exports = Task;
