9ea3b22eabaf6bbe7c4094a180bd455a1b8993d5
krushik bairaboina <113887483+KrushikBairaboina@users.noreply.github.com>
2024-11-28 23:02:21 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const cors = require('cors');
-const TaskRoutes = require('./Routes/TaskRoutes');  // Assuming you have TaskRoutes defined
+const TaskRoutes = require('./Routes/TaskRoutes');  
 const PORT = process.env.PORT || 8080;
 
-require('./Models/db');  // Ensure your database is correctly connected
+require('./Models/db');  
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Use /api/tasks route
 app.use('/api/tasks', TaskRoutes);
 
 app.listen(PORT, () => {
